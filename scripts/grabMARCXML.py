65181dfe4abc7034fad5283fc72680da8ebea870
Christina Harlow <cmharlow@gmail.com>
2017-01-18 13:31:14 -0500

    Committing n97108612 to GH marcxml branch.

--- scripts/grabMARCXML.py
+++ scripts/grabMARCXML.py
@@ -3,20 +3,27 @@ import requests
 import pushToGH
 
 lccn_url = "https://lccn.loc.gov/{0}/marcxml"
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) \
+           AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 \
+           Safari/537.36'}
 URIs = set()
 
-with open("../identifiers/names.list") as fi:
+with open("identifiers/names.list") as fi:
     for line in fi:
         URIs.add(line.strip('\n'))
+print("Processing %i URIs." % len(URIs))
 
-print("Processing %i URIs.")
 
+pushToGH.setBranch("marcxml")
 for URI in URIs:
-    lccn = URI.replace('http://id.loc.gov/authorities/names/', '')
+    lccn = URI.replace('http://id.loc.gov/authorities/names/', '').strip('>')
     lccn_req = lccn_url.format(lccn)
     try:
-        resp = requests.get(lccn_req).content
-        file_out = lccn + ".marcxml.xml"
-        with open('data/' + file_out, 'w') as out:
+        resp = requests.get(lccn_req, headers=headers).text
+        file_out = 'data/' + lccn + ".marcxml.xml"
+        with open(file_out, 'w') as out:
             out.write(resp)
         pushToGH.commitToGH("marcxml", lccn)
+    except Exception as e:
+        print("Error with %s : %s" % (lccn, e))
+        continue
