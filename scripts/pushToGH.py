adfdbcfd26c8e25f1863a6cab2c7d2ab7e2f3767
Christina Harlow <cmharlow@gmail.com>
2017-01-18 13:31:14 -0500

    merging script changes from marcxml branch

--- scripts/pushToGH.py
+++ scripts/pushToGH.py
@@ -5,11 +5,14 @@ from config import settings
 gh_user = settings["github"]["user"]
 gh_password = settings["github"]["password"]
 gh_repo = settings["github"]["repo"]
+git = sh.git.bake(_cwd=gh_repo)
+
+
+def setBranch(branch):
+    git.checkout(branch)
 
 
 def commitToGH(branch, identifier):
-    git = sh.git.bake(_cwd=gh_repo)
-    git.checkout('-b', branch)
     git.add('.')
     msg = "Committing %s to GH %s branch." % (identifier, branch)
     git.commit(m=msg)
